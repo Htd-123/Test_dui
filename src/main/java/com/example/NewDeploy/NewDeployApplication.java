1af56f8f414ffd6508c3d4cae42b73dfffad66ba
TestNG <huynhthanhdanh9a4@gmail.com>
2024-11-22 16:03:56 +0700

    first commit

---
+++ src/main/java/com/example/NewDeploy/NewDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDeployApplication.class, args);
+	}
+
+}
